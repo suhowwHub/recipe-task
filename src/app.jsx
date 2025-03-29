fa28b476c61c89e124a79d596dee3a20401b81e8
Serafim Suhov <serafimsuhov@gmail.com>
2025-03-29 10:57:07 +0300

    opting out of module pagination steps

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react"
 import styles from "./app.module.css"
 import data from "./data.json"
+import { PaginationStep } from "./pagination-step"
 
 export const App = () => {
+	const [steps, setSteps] = useState(data)
+
+	const paginationList = steps.map((step, index) => (
+		<li className={styles["steps-item"]}>
+			<button className={styles["steps-item-button"]}>{index + 1}</button>
+			{step.title}
+		</li>
+	))
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.card}>
@@ -11,27 +21,7 @@ export const App = () => {
 					<div className={styles["steps-content"]}>
 						Контент соответственный шагу. Сейчас активен шаг 3
 					</div>
-					<ul className={styles["steps-list"]}>
-						<li className={styles["steps-item"] + " " + styles.done}>
-							<button className={styles["steps-item-button"]}>1</button>
-							Шаг 1
-						</li>
-						<li className={styles["steps-item"] + " " + styles.done}>
-							<button className={styles["steps-item-button"]}>2</button>
-							Шаг 2
-						</li>
-						<li
-							className={
-								styles["steps-item"] + " " + styles.done + " " + styles.active
-							}>
-							<button className={styles["steps-item-button"]}>3</button>
-							Шаг 3
-						</li>
-						<li className={styles["steps-item"]}>
-							<button className={styles["steps-item-button"]}>4</button>
-							Шаг 4
-						</li>
-					</ul>
+					<ul className={styles["steps-list"]}>{paginationList}</ul>
 					<div className={styles["buttons-container"]}>
 						<button className={styles.button}>Назад</button>
 						<button className={styles.button}>Далее</button>
