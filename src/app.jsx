699db0addd65511b781c6f8db1b239a04d5f2d36
Serafim Suhov <serafimsuhov@gmail.com>
2025-03-29 17:06:19 +0300

    ready-made solution

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,63 @@
 import { useState } from "react"
 import styles from "./app.module.css"
 import data from "./data.json"
-import { PaginationStep } from "./pagination-step"
 
 export const App = () => {
-	const [steps, setSteps] = useState(data)
+	const [steps] = useState(data)
+	const [activeIndex, setActiveIndex] = useState(0)
+	const isFirstStep = activeIndex === 0
+	const isLastStep = activeIndex === steps.length - 1
+	const content = steps[activeIndex].content
 
-	const paginationList = steps.map((step, index) => (
-		<li className={styles["steps-item"]}>
-			<button className={styles["steps-item-button"]}>{index + 1}</button>
-			{step.title}
-		</li>
-	))
+	const prevButtonHendler = () => {
+		if (!isFirstStep) setActiveIndex((currentIndex) => currentIndex - 1)
+	}
+	const nextButtonHendler = () => {
+		if (!isLastStep) setActiveIndex((currentIndex) => currentIndex + 1)
+		else resetHendler()
+	}
+	const paginationHendler = (index) => setActiveIndex(index)
+	const resetHendler = () => setActiveIndex(0)
+
+	const setClasses = (index, activeIndex) => {
+		return [
+			// prettier-ignore
+			styles["steps-item"],
+			index < activeIndex ? styles.done : " ",
+			index === activeIndex ? styles.active : " ",
+		]
+			.join(" ")
+			.trim()
+	}
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.card}>
 				<h1>Инструкция по готовке пельменей</h1>
 				<div className={styles.steps}>
-					<div className={styles["steps-content"]}>
-						Контент соответственный шагу. Сейчас активен шаг 3
-					</div>
-					<ul className={styles["steps-list"]}>{paginationList}</ul>
+					<div className={styles["steps-content"]}>{content}</div>
+					<ul className={styles["steps-list"]}>
+						{steps.map((step, index) => (
+							<li className={`${setClasses(index, activeIndex)}`} key={step.id}>
+								<button
+									className={styles["steps-item-button"]}
+									onClick={() => paginationHendler(index)}>
+									{index + 1}
+								</button>
+								{step.title}
+							</li>
+						))}
+					</ul>
 					<div className={styles["buttons-container"]}>
-						<button className={styles.button}>Назад</button>
-						<button className={styles.button}>Далее</button>
+						<button
+							className={styles.button}
+							disabled={isFirstStep}
+							onClick={prevButtonHendler}>
+							Назад
+						</button>
+						<button className={styles.button} onClick={nextButtonHendler}>
+							{isLastStep ? "Начать сначала" : "Далее"}
+						</button>
 					</div>
 				</div>
 			</div>
